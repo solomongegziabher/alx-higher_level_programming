7f2eda154bb60fdb467b322cbfdfb7fbd3676260
solomongegziabher <sage.h22@gmail.com>
2023-01-04 23:18:07 -0800

    if else loop funcn

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-=======
 
->>>>>>> edc8180a01b7b7ef997129f70dd2155dad2a96a6
-i = 0
+i = o
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
