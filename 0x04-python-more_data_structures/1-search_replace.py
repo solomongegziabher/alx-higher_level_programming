848eaa33a09e69e4faf5e266f522cb7ae8fd2c5e
solomongegziabher <sage.h22@gmail.com>
2023-01-11 03:36:02 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for k in range(len(my_list)):
+        if my_list[k] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[k])
+    return new_lists
