43ff0d14431227234594a9f4642cbbfc1de2b903
solomongegziabher <sage.h22@gmail.com>
2023-02-07 03:48:39 -0800

    Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """ Class to create student instances """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        obj = self.__dict__.copy()
-        if type(attrs) is list:
-
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
-
-            d_list = {}
-
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return 
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__ 
